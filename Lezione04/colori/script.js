a1c7ded91e7207bd32efa408072489504004ba9e
maboglia <mauro.bogliaccino@gmail.com>
2022-11-23 13:55:46 +0100

    agg

--- Lezione04/colori/script.js
+++ Lezione04/colori/script.js
@@ -1,12 +1,24 @@
 const colori = ['red', 'green', 'blue', '#FFCC33']
 
 const TENDINA = document.createElement('select')
+const INPUT = document.createElement('input')
+INPUT.setAttribute('placeholder', "Aggiungi un colore")
+const BTN = document.createElement('button')
+BTN.textContent = "Add colore";
+BTN.onclick = function () {
+    colori.push(INPUT.value)
+    coloraSfondo()
+}
 
-for (const colore of colori) {
-    const OPTION = document.createElement('option')
-    OPTION.textContent = colore
-    TENDINA.appendChild(OPTION)
-    console.log(colore);
+
+function coloraSfondo() {
+    TENDINA.innerHTML = ''
+    for (const colore of colori) {
+        const OPTION = document.createElement('option')
+        OPTION.textContent = colore
+        TENDINA.appendChild(OPTION)
+        console.log(colore);
+    }
 }
 
 TENDINA.onchange = function(e){
@@ -15,3 +27,5 @@ TENDINA.onchange = function(e){
 }
 
 document.body.appendChild(TENDINA)
+document.body.appendChild(INPUT)
+document.body.appendChild(BTN)
